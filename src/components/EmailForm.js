bd219985c68575e611d663c6c3b0c850a00ac128
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-30 18:19:42 -0500

    Refactor form to subscribe users to mailchimp

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,28 +1,67 @@
 import React, { useState } from 'react';
+import addToMailchimp from 'gatsby-plugin-mailchimp';
+import { useForm } from '../hooks';
 
 const EmailForm = () => {
-  const [message, setMessage] = useState('')
+  const [mailchimpResult, setMailchimpResult] = useState({ result: '', msg: '' });
+  const [isDisabled, setIsDisabled] = useState(false);
+  const { handleChange, handleSubmit, values } = useForm(handleSuccess);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setMessage('Thank you!');
+  async function handleSuccess() {
+    const result = await addToMailchimp(values.email, { NAME: values.name });
+    setMailchimpResult(res => {
+      return {...result, msg: fixMailchimpMessage(result.msg)};
+    });
+    setIsDisabled(true);
+
     setTimeout(() => {
-      setMessage('');
+      setMailchimpResult({ error: '', msg: '' });
+      setIsDisabled(false);
     }, 3000);
   }
 
+  function getMessageClassName() {
+    switch (mailchimpResult.result) {
+      case 'success':
+        return 'visible success';
+      case 'error':
+        return 'visible error';
+      default:
+        return '';
+    }
+  }
+
+  function fixMailchimpMessage(msg) {
+    if (msg.includes('subscribed')) {
+      return `${values.email} is already subscribed.`
+    }
+
+    return msg;
+  }
+
   return (
-    <form id="signup-form" onSubmit={onSubmit} method="post" action="#">
+    <form id="signup-form" onSubmit={handleSubmit} method="post" action="#">
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Name"
+        onChange={handleChange}
+        value={values.name}
+        required
+      />
       <input
         type="email"
         name="email"
         id="email"
         placeholder="Enter email for updates"
+        onChange={handleChange}
+        value={values.email}
+        required
       />
-      <input type="submit" value="Sign Up" />
-      <span className={`${message ? 'visible success' : ''} message`}>
-        {message}
+      <input type="submit" value="Subscribe" disabled={isDisabled} />
+      <span className={`${getMessageClassName()} message`}>
+        {mailchimpResult.msg}
       </span>
     </form>
   );
