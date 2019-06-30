c57904235199e715d28d406ce8e47187b25b5b06
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-30 13:28:16 -0500

    Refactor to use useStaticQuery hook

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import '../assets/sass/main.scss';
 
 const Layout = ({ children }) => {
   const [isPreloaded, setIsPreloaded] = useState(true);
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,34 +24,21 @@ const Layout = ({ children }) => {
   }, []);
     
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Eventually' },
-              { name: 'keywords', content: 'site, web' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
-            {children}
-          </div>
-        </>
-      )}
-    />
-  );
+    <>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Coming Soon' },
+          { name: 'keywords', content: 'site, web' },
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
+        {children}
+      </div>
+    </>
+  )
 }
 
 Layout.propTypes = {
