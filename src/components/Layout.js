496c7773ed5ed6134d4c75a10f0809b6462b0a07
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-30 12:59:15 -0500

    Use hooks and functional component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,57 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
 import '../assets/sass/main.scss';
 
-class Layout extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isPreloaded: true,
-    };
-  }
+const Layout = ({ children }) => {
+  const [isPreloaded, setIsPreloaded] = useState(true);
 
-  componentDidMount() {
+  useEffect(() => {
     setTimeout(() => {
-      this.setState({ isPreloaded: false });
+      setIsPreloaded(false);
     }, 100);
-  }
-
-  render() {
-    const { children } = this.props;
-    const { isPreloaded } = this.state;
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
+  }, []);
+    
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
             }
           }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Eventually' },
-                { name: 'keywords', content: 'site, web' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
-              {children}
-            </div>
-          </>
-        )}
-      />
-    );
-  }
+        }
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Eventually' },
+              { name: 'keywords', content: 'site, web' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
+            {children}
+          </div>
+        </>
+      )}
+    />
+  );
 }
 
 Layout.propTypes = {
