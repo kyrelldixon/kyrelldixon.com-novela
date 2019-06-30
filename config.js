460480018e7cf8e8b5bc65bb61194317c5668f21
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-30 12:07:12 -0500

    Initial commit from gatsby: (https://github.com/anubhavsrivastava/gatsby-starter-eventually.git)

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Eventually', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Eventually',
+  subHeading:
+    "A simple template for telling the world when you'll launch your next big thing.",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
