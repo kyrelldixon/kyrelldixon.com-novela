a5d0e84208cfcf44899e4daa09878030f320a41b
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-03 20:16:23 -0500

    Minor fix on Logo closing tag

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -7,10 +7,10 @@ const Logo = () => {
   
   return (
     <svg width="18" height="23" viewBox="0 0 18 23" fill="none" xmlns="http://www.w3.org/2000/svg" className="Logo__Desktop">
-      <path d="M17.5907 20.2954H0V23H17.5907V20.2954Z" fill={isDark ? "#fff" : "#000"}></path>
-      <path d="M0 7.96484V18.9537L5.38796 15.1843V11.7343L0 7.96484Z" fill={isDark ? "#fff" : "#000"}></path>
-      <path d="M17.5689 10.9463V0L12.1597 3.74815V7.13426L17.5689 10.9463Z" fill={isDark ? "#fff" : "#000"}></path>
-      <path d="M17.5907 18.975L17.5694 12.288L0 0V6.62315L17.5907 18.975Z" fill={isDark ? "#fff" : "#000"}></path>
+      <path d="M17.5907 20.2954H0V23H17.5907V20.2954Z" fill={isDark ? "#fff" : "#000"} />
+      <path d="M0 7.96484V18.9537L5.38796 15.1843V11.7343L0 7.96484Z" fill={isDark ? "#fff" : "#000"} />
+      <path d="M17.5689 10.9463V0L12.1597 3.74815V7.13426L17.5689 10.9463Z" fill={isDark ? "#fff" : "#000"} />
+      <path d="M17.5907 18.975L17.5694 12.288L0 0V6.62315L17.5907 18.975Z" fill={isDark ? "#fff" : "#000"} />
     </svg>
   )
 }
