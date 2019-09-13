1b673deca002f8d6e6edad963ac8fbc69c7e8440
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-13 17:06:09 -0500

    add logo no styling

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -1,19 +1,22 @@
-import React from "react"
+import React from "react";
+import styled from "@emotion/styled";
 import { useColorMode } from "theme-ui";
+
+import logo from '../../../../assets/kd6 logo.svg';
 
 const Logo = () => {
   const [colorMode, ] = useColorMode();
   const isDark = colorMode === `dark`;
   
   return (
-    <svg width="18" height="23" viewBox="0 0 18 23" fill="none" xmlns="http://www.w3.org/2000/svg" className="Logo__Desktop">
-      <path d="M17.5907 20.2954H0V23H17.5907V20.2954Z" fill={isDark ? "#fff" : "#000"} />
-      <path d="M0 7.96484V18.9537L5.38796 15.1843V11.7343L0 7.96484Z" fill={isDark ? "#fff" : "#000"} />
-      <path d="M17.5689 10.9463V0L12.1597 3.74815V7.13426L17.5689 10.9463Z" fill={isDark ? "#fff" : "#000"} />
-      <path d="M17.5907 18.975L17.5694 12.288L0 0V6.62315L17.5907 18.975Z" fill={isDark ? "#fff" : "#000"} />
-    </svg>
+    <div>
+      <Img src={logo} />
+    </div>
   )
 }
 
+const Img = styled.img`
+  width: 200px;
+`;
 
 export default Logo
