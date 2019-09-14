a64b1ca1d6b03cf20cddd6960bd7c236475c9d4f
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-13 21:12:34 -0500

    Remove extra logos and make logo responsive

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import styled from "@emotion/styled";
 import { useColorMode } from "theme-ui";
 
-import logo from '../../../../assets/kd6 logo.png';
-import logoDark from '../../../../assets/kd6 logo(dark mode).png';
+import mediaqueries from "@narative/gatsby-theme-novela/src/styles/media";
+
+import logo from '../../../../assets/kd6 logo-blue-gradient-light.png';
+import logoDark from '../../../../assets/kd6 logo-blue-gradient-dark.png';
 
 const Logo = () => {
   const [colorMode, ] = useColorMode();
@@ -18,6 +20,10 @@ const Logo = () => {
 
 const Img = styled.img`
   width: 200px;
+
+  ${mediaqueries.phablet`
+    width: 150px;
+  `}
 `;
 
 export default Logo
